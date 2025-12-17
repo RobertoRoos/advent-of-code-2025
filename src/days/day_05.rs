ced3717006180c01282c9aed4dd3141873172e12
Robert <robert.soor@gmail.com>
2025-12-17 13:30:38 +0100

    Working on d5p9 - Tests are passing but final answer is wrong

--- src/days/day_05.rs
+++ src/days/day_05.rs
@@ -17,8 +17,22 @@ impl Solution for Day05 {
         Outcome::I32(count as i32)
     }
 
-    fn run_part_2(&self, _input_file: PathBuf) -> Outcome {
-        Outcome::Text(String::from("d5p2"))
+    fn run_part_2(&self, input_file: PathBuf) -> Outcome {
+        let mut lines = self.get_file_reader(input_file).lines();
+        let new_ranges = self.make_ranges(lines.by_ref());
+
+        let mut combined_ranges: Vec<RangeInclusive<u64>> = Vec::new();
+
+        for range in new_ranges {
+            combined_ranges = Self::combine_inclusive_range(combined_ranges, range);
+        }
+
+        let result: u64 = combined_ranges
+            .into_iter()
+            .map(|r| r.end() + 1 - r.start())
+            .sum();
+        // Avoid `count()` as it will perform the actual iteration, which we don't need
+        Outcome::I32(result as i32)
     }
 }
 
@@ -33,6 +47,37 @@ impl Day05 {
             .map(|line| self.get_range_from_line(&line.unwrap()))
             .collect()
     }
+
+    /// Merge a set of existing ranges with a new one
+    fn combine_inclusive_range(
+        mut ranges: Vec<RangeInclusive<u64>>,
+        new_range: RangeInclusive<u64>,
+    ) -> Vec<RangeInclusive<u64>> {
+        if new_range.start() == new_range.end() {
+            return ranges; // We don't actually iterate our ranges, so this is valid
+        }
+        for (this_i, this_range) in ranges.iter().enumerate() {
+            if this_range.contains(new_range.start()) && this_range.contains(new_range.end()) {
+                // `new_range` falls entirely within `this_range`, so no need to do anything
+                return ranges;
+            } else if this_range.contains(new_range.start())
+                || this_range.contains(new_range.end())
+                || new_range.start() < this_range.start() && new_range.end() > this_range.end()
+            {
+                // We have any amount of overlap of `new_range` and `this_range`, but not full
+                // We replace `new_range` by a sum of `new_range` and `this_range` and remove
+                // `this_range` from the list of already processed ranges.
+                let new_new_range = *this_range.start().min(new_range.start())
+                    ..=*this_range.end().max(new_range.end());
+                // Our modified `new_range` could overlap with other existing ranges, hence we
+                // put it though the loops again.
+                ranges.remove(this_i);
+                return Self::combine_inclusive_range(ranges, new_new_range);
+            }
+        }
+        ranges.push(new_range);
+        ranges
+    }
 }
 
 #[cfg(test)]
@@ -40,14 +85,43 @@ mod tests {
     use super::*;
 
     #[test]
-    fn part_1_sample() {
+    fn test_part_1_sample() {
         let solver = Day05 {};
         let result = solver.run_part_1(PathBuf::from("tests/day_05/sample.txt"));
         assert_eq!(result, Outcome::I32(3));
     }
 
     #[test]
-    fn part_2_sample() {
+    fn test_combine_inclusive_range() {
+        let mut ranges = Vec::new();
+        ranges = Day05::combine_inclusive_range(ranges, 3..=8); // First
+        assert_eq!(ranges, vec![3..=8]);
+
+        ranges = Day05::combine_inclusive_range(ranges, 1..=10); // Full upgrade
+        assert_eq!(ranges, vec![1..=10]);
+
+        ranges = Day05::combine_inclusive_range(ranges, 16..=18); // No overlap
+        assert_eq!(ranges, vec![1..=10, 16..=18]);
+
+        ranges = Day05::combine_inclusive_range(ranges, 3..=5); // Full overlap
+        assert_eq!(ranges, vec![1..=10, 16..=18]);
+
+        ranges = Day05::combine_inclusive_range(ranges, 17..=20); // Partial overlap
+        assert_eq!(ranges, vec![1..=10, 16..=20]);
+
+        ranges = Day05::combine_inclusive_range(ranges, 5..=19); // Dual overlap
+        assert_eq!(ranges, vec![1..=20]);
+
+        // Tiple overlap:
+        ranges = Day05::combine_inclusive_range(ranges, 25..=30);
+        ranges = Day05::combine_inclusive_range(ranges, 35..=40);
+        assert_eq!(ranges, vec![1..=20, 25..=30, 35..=40]);
+        ranges = Day05::combine_inclusive_range(ranges, 18..=35);
+        assert_eq!(ranges, vec![1..=40]);
+    }
+
+    #[test]
+    fn test_part_2_sample() {
         let solver = Day05 {};
         let result = solver.run_part_2(PathBuf::from("tests/day_05/sample.txt"));
         assert_eq!(result, Outcome::I32(14));
