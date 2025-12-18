33cffc591cd4f0f3555cf518ce56d2179e8d82d7
Robert <robert.soor@gmail.com>
2025-12-18 15:17:11 +0100

    Got d6p2 but a little slow (550ms)

--- src/days/day_06.rs
+++ src/days/day_06.rs
@@ -1,5 +1,6 @@
 use crate::shared::{Outcome, Solution};
 use std::io::BufRead;
+use std::ops::Range;
 use std::path::PathBuf;
 
 pub struct Day06;
@@ -31,17 +32,51 @@ impl Solution for Day06 {
             }
         }
 
-        let sum = numbers
-            .iter()
-            .enumerate()
-            .map(|(column_idx, column)| Self::column_operation(column, operators[column_idx]))
-            .sum();
-
-        Outcome::U64(sum)
+        Outcome::U64(Self::sum_column_operations(&numbers, &operators))
     }
 
-    fn run_part_2(&self, _input_file: PathBuf) -> Outcome {
-        Outcome::Text(String::from("d6p2"))
+    fn run_part_2(&self, input_file: PathBuf) -> Outcome {
+        let mut lines: Vec<String> = self
+            .get_file_reader(input_file)
+            .lines()
+            .map(|line| line.unwrap())
+            .collect();
+        let max_line_length: usize = lines.iter().map(|line| line.chars().count()).max().unwrap();
+
+        let operators_line = lines.pop().unwrap();
+        let mut column_ranges: Vec<Range<usize>> = Vec::new();
+        let mut operators: Vec<char> = Vec::new();
+        let mut last_idx = None;
+        for (column_idx, c) in operators_line.chars().enumerate() {
+            if c != ' ' {
+                operators.push(c);
+                if let Some(last_idx) = last_idx {
+                    column_ranges.push(last_idx..(column_idx - 1));
+                }
+                last_idx = Some(column_idx);
+            }
+        }
+        if let Some(last_idx) = last_idx {
+            column_ranges.push(last_idx..max_line_length); // Last range will be missing
+        }
+
+        let numbers: Vec<Vec<u64>> = column_ranges
+            .into_iter()
+            .map(|column_range| {
+                column_range
+                    .into_iter()
+                    .map(|pos| {
+                        let num_str = lines
+                            .iter()
+                            .map(|line| line.chars().nth(pos).unwrap_or(' '))
+                            .collect::<String>();
+                        num_str.trim().parse::<u64>().unwrap()
+                    })
+                    .collect()
+            })
+            .collect();
+
+        Outcome::U64(Self::sum_column_operations(&numbers, &operators))
     }
 }
 
@@ -53,6 +88,15 @@ impl Day06 {
             '*' => column.iter().product::<u64>(),
             _ => panic!("Unrecognized character"),
         }
+    }
+
+    /// Sum operations on all columns at once
+    fn sum_column_operations(columns: &[Vec<u64>], operators: &[char]) -> u64 {
+        columns
+            .iter()
+            .enumerate()
+            .map(|(column_idx, column)| Self::column_operation(column, operators[column_idx]))
+            .sum()
     }
 }
 
