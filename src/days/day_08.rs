f4ca1cfde44d2030eef1685a4ccd2f9834321dd1
Robert <robert.soor@gmail.com>
2026-01-10 14:35:55 +0100

    Test for d8p2 is succeeding

--- src/days/day_08.rs
+++ src/days/day_08.rs
@@ -1,5 +1,4 @@
 use crate::shared::{Outcome, Solution};
-use std::cmp::{max, min};
 use std::collections::HashSet;
 use std::hash::Hash;
 use std::io::BufRead;
@@ -15,6 +14,45 @@ pub struct Day08 {
 
 impl Solution for Day08 {
     fn run_part_1(&self, input_file: PathBuf) -> Outcome {
+        let (_junctions, distances, mut circuits) = self.find_distances_and_circuits(input_file);
+
+        // Get the shortest distances (up to limit):
+        for (pair, _dist) in distances.iter().take(self.limit) {
+            Self::merge_circuits(pair, &mut circuits);
+        }
+
+        // Find the largest three circuits to multiply the size of:
+        let mut circuit_sizes: Vec<u64> = circuits.iter().map(|c| c.len() as u64).collect();
+        circuit_sizes.sort_by(|a, b| b.cmp(a));
+
+        let result = circuit_sizes.iter().take(3).product();
+        Outcome::U64(result)
+    }
+
+    fn run_part_2(&self, input_file: PathBuf) -> Outcome {
+        let (junctions, distances, mut circuits) = self.find_distances_and_circuits(input_file);
+
+        // Get the shortest distances (up to limit):
+        for (pair, _dist) in distances {
+            Self::merge_circuits(&pair, &mut circuits);
+
+            if circuits.len() == 1 {
+                // Everything just became a single circuit!
+                return Outcome::U64(
+                    u64::try_from(junctions[pair.0][0] * junctions[pair.1][0]).unwrap(),
+                );
+            }
+        }
+
+        panic!("Failed to find solution");
+    }
+}
+
+impl Day08 {
+    fn find_distances_and_circuits(
+        &self,
+        input_file: PathBuf,
+    ) -> (Points, Vec<(Pair, f64)>, Vec<HashSet<usize>>) {
         let junctions: Points = self
             .get_file_reader(input_file)
             .lines()
@@ -29,7 +67,7 @@ impl Solution for Day08 {
             .collect();
 
         // Track circuits (= groups of connected junctions):
-        let mut circuits: Vec<HashSet<usize>> = junctions
+        let circuits: Vec<HashSet<usize>> = junctions
             .iter()
             .enumerate()
             .map(|(idx, _)| HashSet::from([idx]))
@@ -37,29 +75,9 @@ impl Solution for Day08 {
 
         let distances = Self::find_closest_pairs(&junctions);
 
-        // Get the shortest distances:
-        for (pair, _dist) in distances.iter().take(self.limit) {
-            let idx_1 = Self::find_in_sets(max(&pair.0, &pair.1), &circuits).unwrap();
-            let idx_2 = Self::find_in_sets(min(&pair.0, &pair.1), &circuits).unwrap();
-
-            let moving: Vec<usize> = circuits[idx_2].drain().collect();
-            circuits[idx_1].extend(moving);
-        }
-
-        // Find the largest three circuits to multiply the size of:
-        let mut circuit_sizes: Vec<u64> = circuits.iter().map(|c| c.len() as u64).collect();
-        circuit_sizes.sort_by(|a, b| b.cmp(a));
-
-        let result = circuit_sizes.iter().take(3).product();
-        Outcome::U64(result)
+        (junctions, distances, circuits)
     }
 
-    fn run_part_2(&self, _input_file: PathBuf) -> Outcome {
-        Outcome::Text(String::from("d8p2"))
-    }
-}
-
-impl Day08 {
     /// Make a list of pairs and their distances and sort them
     fn find_closest_pairs(points: &[Point]) -> Vec<(Pair, f64)> {
         let mut distances: Vec<(Pair, f64)> = Vec::new();
@@ -94,78 +112,43 @@ impl Day08 {
         }
         None
     }
+
+    /// From a pair, combine the two corresponding circuits
+    fn merge_circuits(pair: &Pair, circuits: &mut Vec<HashSet<usize>>) {
+        let mut circuit_ids: Vec<usize> = [pair.0, pair.1]
+            .iter()
+            .map(|idx| Self::find_in_sets(idx, circuits).unwrap())
+            .collect();
+
+        if circuit_ids[0] == circuit_ids[1] {
+            return; // Nothing changes
+        }
+
+        circuit_ids.sort_unstable(); // Sort, such that the lowest index won't be affected by the removal
+        // of the higher index.
+
+        let circuit_old = circuits.swap_remove(circuit_ids[1]); // Move out of the vector
+
+        circuits[circuit_ids[0]].extend(circuit_old);
+        // Move the elements into the first circuit, inside the vector
+    }
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
 
-    // #[test]
-    // fn test_find_closest_pair() {
-    //     let points = [
-    //         [5, 5, 5],
-    //         [1, 1, 1],
-    //         [25, 25, 25],
-    //         [45, 45, 45],
-    //         [15, 15, 15],
-    //         [2, 2, 2],
-    //         [90, 90, 90],
-    //         [190, 190, 190],
-    //         [290, 290, 290],
-    //         [390, 390, 390],
-    //     ];
-    //     let (pair, dist) = Day08::find_closest_pair(&points);
-    //
-    //     assert_eq!(pair, (1, 5));
-    //     assert_eq!(dist, 3_f64.sqrt());
-    // }
-
-    // #[test]
-    // fn test_find_closest_pair_from_sample() {
-    //     let points = [
-    //         [162, 817, 812],
-    //         [57, 618, 57],
-    //         [906, 360, 560],
-    //         [592, 479, 940],
-    //         [352, 342, 300],
-    //         [466, 668, 158],
-    //         [542, 29, 236],
-    //         [431, 825, 988],
-    //         [739, 650, 466],
-    //         [52, 470, 668],
-    //         [216, 146, 977],
-    //         [819, 987, 18],
-    //         [117, 168, 530],
-    //         [805, 96, 715],
-    //         [346, 949, 466],
-    //         [970, 615, 88],
-    //         [941, 993, 340],
-    //         [862, 61, 35],
-    //         [984, 92, 344],
-    //         [425, 690, 689],
-    //     ];
-    //     let (pair, _) = Day08::find_closest_pair(&points);
-    //     assert_eq!(pair, (0, 19));
-    // }
-
-    // #[test]
-    // fn test_find_closest_pair_across() {
-    //     let points_left = [(10, [1, 1, 1]), (11, [2, 2, 2]), (12, [3, 3, 3])];
-    //     let points_right = [
-    //         (13, [4, 4, 4]),
-    //         (14, [5, 5, 5]),
-    //         (15, [6, 6, 6]),
-    //         (16, [7, 7, 7]),
-    //     ];
-    //     let result = Day08::find_closest_pair_across(&points_left, &points_right, 100_f64);
-    //     assert!(result.is_some());
-    //     assert_eq!(result.unwrap().0, (12, 13));
-    // }
-
     #[test]
     fn test_part_1_sample() {
         let solver = Day08 { limit: 10 };
         let result = solver.run_part_1(PathBuf::from("tests/day_08/sample.txt"));
         assert_eq!(result, Outcome::U64(40));
+    }
+
+    #[test]
+    fn test_part_2_sample() {
+        let solver = Day08 { limit: 10 };
+        let result = solver.run_part_2(PathBuf::from("tests/day_08/sample.txt"));
+        assert_eq!(result, Outcome::U64(25272));
     }
 }
